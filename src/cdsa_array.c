04feab7d8e932c26fac00ef085a18187222708dc
ddddddeon <chris.deon@unity3d.com>
2023-04-29 18:33:37 -0400

    tweak macro

--- src/cdsa_array.c
+++ src/cdsa_array.c
@@ -11,7 +11,7 @@ typedef struct {
     int data[];
 } cdsa_array;
 
-#define ABORT_IF_FREED                                                              \
+#define ABORT_IF_FREED(c)                                               \
     do {                                                                            \
         if (c->freed) {                                                             \
             printf("[%s:%d] cdsa_array has been freed!\n", __FUNCTION__, __LINE__); \
@@ -28,15 +28,15 @@ cdsa_array *cdsa_array_new(size_t len) {
 }
 
 void cdsa_array_free(cdsa_array *c) {
-    free(c);
     c->freed = true;
+    free(c);
 }
 
 size_t cdsa_array_size(cdsa_array *c) { return c->size; }
 size_t cdsa_array_cap(cdsa_array *c) { return c->cap; }
 
 void cdsa_array_display(cdsa_array *c) {
-    ABORT_IF_FREED;
+    ABORT_IF_FREED(c);
     printf("[ ");
     for (int i = 0; i < c->size; i++) {
         printf("%d ", c->data[i]);
@@ -45,12 +45,12 @@ void cdsa_array_display(cdsa_array *c) {
 }
 
 int cdsa_array_avail(cdsa_array *c) {
-    ABORT_IF_FREED;
+    ABORT_IF_FREED(c);
     return c->cap - c->size;
 }
 
 int cdsa_array_insert(cdsa_array *c, int n) {
-    ABORT_IF_FREED;
+    ABORT_IF_FREED(c);
     if (cdsa_array_avail(c) <= 0) {
         return -1;
     }
@@ -62,7 +62,7 @@ int cdsa_array_insert(cdsa_array *c, int n) {
 }
 
 int cdsa_array_insert_at(cdsa_array *c, int n, int i) {
-    ABORT_IF_FREED;
+    ABORT_IF_FREED(c);
     if (c->cap - c->size < 1 || i >= c->cap) {
         return -1;
     }
@@ -78,7 +78,7 @@ int cdsa_array_insert_at(cdsa_array *c, int n, int i) {
 }
 
 int cdsa_array_update_at(cdsa_array *c, int n, int i) {
-    ABORT_IF_FREED;
+    ABORT_IF_FREED(c);
     if (i >= c->cap) {
         return -1;
     }
@@ -88,7 +88,7 @@ int cdsa_array_update_at(cdsa_array *c, int n, int i) {
 }
 
 int cdsa_array_delete_at(cdsa_array *c, int i) {
-  ABORT_IF_FREED;
+    ABORT_IF_FREED(c);
   if (i >= c->size) {
     return -1;
   }
@@ -101,7 +101,7 @@ int cdsa_array_delete_at(cdsa_array *c, int i) {
 }
 
 void cdsa_array_fill(cdsa_array *c, int n) {
-    ABORT_IF_FREED;
+    ABORT_IF_FREED(c);
     c->size = 0;
     for (int i = 0; i < c->cap; i++) {
         *(c->data + i) = n;
